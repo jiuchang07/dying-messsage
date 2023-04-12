61f36cb20a1dd2da330513578778b8428fc9757d
jiuchang07 <jiuchang99@gmail.com>
2023-04-11 17:10:54 -0700

    fixed a small bug in the previous commit (6ec425)

--- src/backend/rooms/schema/Component.ts
+++ src/backend/rooms/schema/Component.ts
@@ -129,288 +129,288 @@ export const getRandomOccupations = () => {
 
 
 // Library of different options for different components
-const MOTIVES = [
-    class Inheritance extends Option {
-        value = "Inheritance";
-        isInGame = true;
-        isExcluded = false;
-        isSolution = false;
-        type = "motives";
-        constructor() {
-            super();
-        }
-    },
-    class Fun extends Option {
-        value = "Fun";
-        isInGame = true;
-        isExcluded = false;
-        isSolution = false;
-        type = "motives";
-        constructor() {
-            super();
-        }
-    },
-    class Robbery extends Option {
-        value = "Robbery";
-        isInGame = true;
-        isExcluded = false;
-        isSolution = false;
-        type = "motives";
-        constructor() {
-            super();
-        }
-    },
-    class Stalking extends Option {
-        value = "Stalking";
-        isInGame = true;
-        isExcluded = false;
-        isSolution = false;
-        type = "motives";
-        constructor() {
-            super();
-        }
-    },
-    class Cheating extends Option {
-        value = "Cheating";
-        isInGame = true;
-        isExcluded = false;
-        isSolution = false;
-        type = "motives";
-        constructor() {
-            super();
-        }
-    },
-    class Fraud extends Option {
-        value = "Fraud";
-        isInGame = true;
-        isExcluded = false;
-        isSolution = false;
-        type = "motives";
-        constructor() {
-            super();
-        }
-    },
-    class LifeInsurance extends Option {
-        value = "Life Insurance";
-        isInGame = true;
-        isExcluded = false;
-        isSolution = false;
-        type = "motives";
-        constructor() {
-            super();
-        }
-    },
-    class DomesticViolence extends Option {
-        value = "Domestic Violence";
-        isInGame = true;
-        isExcluded = false;
-        isSolution = false;
-        type = "motives";
-        constructor() {
-            super();
-        }
-    },
-    class Debt extends Option {
-        value = "Debt";
-        isInGame = true;
-        isExcluded = false;
-        isSolution = false;
-        type = "motives";
-        constructor() {
-            super();
-        }
-    },
-    class Envy extends Option {
-        value = "Envy";
-        isInGame = true;
-        isExcluded = false;
-        isSolution = false;
-        type = "motives";
-        constructor() {
-            super();
-        }
-    },
-    class Betrayal extends Option {
-        value = "Betrayal";
-        isInGame = true;
-        isExcluded = false;
-        isSolution = false;
-        type = "motives";
-        constructor() {
-            super();
-        }
-    },
-    class Threat extends Option {
-        value = "Threat";
-        isInGame = true;
-        isExcluded = false;
-        isSolution = false;
-        type = "motives";
-        constructor() {
-            super();
-        }
-    }
-];
+// const MOTIVES = [
+//     class Inheritance extends Option {
+//         value = "Inheritance";
+//         isInGame = true;
+//         isExcluded = false;
+//         isSolution = false;
+//         type = "motives";
+//         constructor() {
+//             super();
+//         }
+//     },
+//     class Fun extends Option {
+//         value = "Fun";
+//         isInGame = true;
+//         isExcluded = false;
+//         isSolution = false;
+//         type = "motives";
+//         constructor() {
+//             super();
+//         }
+//     },
+//     class Robbery extends Option {
+//         value = "Robbery";
+//         isInGame = true;
+//         isExcluded = false;
+//         isSolution = false;
+//         type = "motives";
+//         constructor() {
+//             super();
+//         }
+//     },
+//     class Stalking extends Option {
+//         value = "Stalking";
+//         isInGame = true;
+//         isExcluded = false;
+//         isSolution = false;
+//         type = "motives";
+//         constructor() {
+//             super();
+//         }
+//     },
+//     class Cheating extends Option {
+//         value = "Cheating";
+//         isInGame = true;
+//         isExcluded = false;
+//         isSolution = false;
+//         type = "motives";
+//         constructor() {
+//             super();
+//         }
+//     },
+//     class Fraud extends Option {
+//         value = "Fraud";
+//         isInGame = true;
+//         isExcluded = false;
+//         isSolution = false;
+//         type = "motives";
+//         constructor() {
+//             super();
+//         }
+//     },
+//     class LifeInsurance extends Option {
+//         value = "Life Insurance";
+//         isInGame = true;
+//         isExcluded = false;
+//         isSolution = false;
+//         type = "motives";
+//         constructor() {
+//             super();
+//         }
+//     },
+//     class DomesticViolence extends Option {
+//         value = "Domestic Violence";
+//         isInGame = true;
+//         isExcluded = false;
+//         isSolution = false;
+//         type = "motives";
+//         constructor() {
+//             super();
+//         }
+//     },
+//     class Debt extends Option {
+//         value = "Debt";
+//         isInGame = true;
+//         isExcluded = false;
+//         isSolution = false;
+//         type = "motives";
+//         constructor() {
+//             super();
+//         }
+//     },
+//     class Envy extends Option {
+//         value = "Envy";
+//         isInGame = true;
+//         isExcluded = false;
+//         isSolution = false;
+//         type = "motives";
+//         constructor() {
+//             super();
+//         }
+//     },
+//     class Betrayal extends Option {
+//         value = "Betrayal";
+//         isInGame = true;
+//         isExcluded = false;
+//         isSolution = false;
+//         type = "motives";
+//         constructor() {
+//             super();
+//         }
+//     },
+//     class Threat extends Option {
+//         value = "Threat";
+//         isInGame = true;
+//         isExcluded = false;
+//         isSolution = false;
+//         type = "motives";
+//         constructor() {
+//             super();
+//         }
+//     }
+// ];
 
-const OCCUPATIONS = [
-    class Musician extends Option {
-        value = "Musician";
-        isInGame = true;
-        isExcluded = false;
-        isSolution = false;
-        type = "occupations";
-        constructor() {
-            super();
-        }
-    },
-    class Bartender extends Option {
-        value = "Bartender";
-        isInGame = true;
-        isExcluded = false;
-        isSolution = false;
-        type = "occupations";
-        constructor() {
-            super();
-        }
-    },
-    class Reporter extends Option {
-        value = "Reporter";
-        isInGame = true;
-        isExcluded = false;
-        isSolution = false;
-        type = "occupations";
-        constructor() {
-            super();
-        }
-    },
-    class Singer extends Option {
-        value = "Singer";
-        isInGame = true;
-        isExcluded = false;
-        isSolution = false;
-        type = "occupations";
-        constructor() {
-            super();
-        }
-    },
-    class Lawyer extends Option {
-        value = "Lawyer";
-        isInGame = true;
-        isExcluded = false;
-        isSolution = false;
-        type = "occupations";
-        constructor() {
-            super();
-        }
-    },
-    class DrugDealer extends Option {
-        value = "Drug Dealer";
-        isInGame = true;
-        isExcluded = false;
-        isSolution = false;
-        type = "occupations";
-        constructor() {
-            super();
-        }
-    },
-    class MovieDirector extends Option {
-        value = "Movie Director";
-        isInGame = true;
-        isExcluded = false;
-        isSolution = false;
-        type = "occupations";
-        constructor() {
-            super();
-        }
-    },
-    class Architect extends Option {
-        value = "Architect";
-        isInGame = true;
-        isExcluded = false;
-        isSolution = false;
-        type = "occupations";
-        constructor() {
-            super();
-        }
-    },
-    class Guard extends Option {
-        value = "Guard";
-        isInGame = true;
-        isExcluded = false;
-        isSolution = false;
-        type = "occupations";
-        constructor() {
-            super();
-        }
-    },
-    class Boxer extends Option {
-        value = "Boxer";
-        isInGame = true;
-        isExcluded = false;
-        isSolution = false;
-        type = "occupations";
-        constructor() {
-            super();
-        }
-    },
-    class Poet extends Option {
-        value = "Poet";
-        isInGame = true;
-        isExcluded = false;
-        isSolution = false;
-        type = "occupations";
-        constructor() {
-            super();
-        }
-    },
-    class Realtor extends Option {
-        value = "Realtor";
-        isInGame = true;
-        isExcluded = false;
-        isSolution = false;
-        type = "occupations";
-        constructor() {
-            super();
-        }
-    },
-    class HairDresser extends Option {
-        value = "Hair Dresser";
-        isInGame = true;
-        isExcluded = false;
-        isSolution = false;
-        type = "occupations";
-        constructor() {
-            super();
-        }
-    },
-    class Doctor extends Option {
-        value = "Doctor";
-        isInGame = true;
-        isExcluded = false;
-        isSolution = false;
-        type = "occupations";
-        constructor() {
-            super();
-        }
-    },
-    class Soldier extends Option {
-        value = "Soldier";
-        isInGame = true;
-        isExcluded = false;
-        isSolution = false;
-        type = "occupations";
-        constructor() {
-            super();
-        }
-    },
-    class Police extends Option {
-        value = "Police";
-        isInGame = true;
-        isExcluded = false;
-        isSolution = false;
-        type = "occupations";
-        constructor() {
-            super();
-        }
-    }
-];
+// const OCCUPATIONS = [
+//     class Musician extends Option {
+//         value = "Musician";
+//         isInGame = true;
+//         isExcluded = false;
+//         isSolution = false;
+//         type = "occupations";
+//         constructor() {
+//             super();
+//         }
+//     },
+//     class Bartender extends Option {
+//         value = "Bartender";
+//         isInGame = true;
+//         isExcluded = false;
+//         isSolution = false;
+//         type = "occupations";
+//         constructor() {
+//             super();
+//         }
+//     },
+//     class Reporter extends Option {
+//         value = "Reporter";
+//         isInGame = true;
+//         isExcluded = false;
+//         isSolution = false;
+//         type = "occupations";
+//         constructor() {
+//             super();
+//         }
+//     },
+//     class Singer extends Option {
+//         value = "Singer";
+//         isInGame = true;
+//         isExcluded = false;
+//         isSolution = false;
+//         type = "occupations";
+//         constructor() {
+//             super();
+//         }
+//     },
+//     class Lawyer extends Option {
+//         value = "Lawyer";
+//         isInGame = true;
+//         isExcluded = false;
+//         isSolution = false;
+//         type = "occupations";
+//         constructor() {
+//             super();
+//         }
+//     },
+//     class DrugDealer extends Option {
+//         value = "Drug Dealer";
+//         isInGame = true;
+//         isExcluded = false;
+//         isSolution = false;
+//         type = "occupations";
+//         constructor() {
+//             super();
+//         }
+//     },
+//     class MovieDirector extends Option {
+//         value = "Movie Director";
+//         isInGame = true;
+//         isExcluded = false;
+//         isSolution = false;
+//         type = "occupations";
+//         constructor() {
+//             super();
+//         }
+//     },
+//     class Architect extends Option {
+//         value = "Architect";
+//         isInGame = true;
+//         isExcluded = false;
+//         isSolution = false;
+//         type = "occupations";
+//         constructor() {
+//             super();
+//         }
+//     },
+//     class Guard extends Option {
+//         value = "Guard";
+//         isInGame = true;
+//         isExcluded = false;
+//         isSolution = false;
+//         type = "occupations";
+//         constructor() {
+//             super();
+//         }
+//     },
+//     class Boxer extends Option {
+//         value = "Boxer";
+//         isInGame = true;
+//         isExcluded = false;
+//         isSolution = false;
+//         type = "occupations";
+//         constructor() {
+//             super();
+//         }
+//     },
+//     class Poet extends Option {
+//         value = "Poet";
+//         isInGame = true;
+//         isExcluded = false;
+//         isSolution = false;
+//         type = "occupations";
+//         constructor() {
+//             super();
+//         }
+//     },
+//     class Realtor extends Option {
+//         value = "Realtor";
+//         isInGame = true;
+//         isExcluded = false;
+//         isSolution = false;
+//         type = "occupations";
+//         constructor() {
+//             super();
+//         }
+//     },
+//     class HairDresser extends Option {
+//         value = "Hair Dresser";
+//         isInGame = true;
+//         isExcluded = false;
+//         isSolution = false;
+//         type = "occupations";
+//         constructor() {
+//             super();
+//         }
+//     },
+//     class Doctor extends Option {
+//         value = "Doctor";
+//         isInGame = true;
+//         isExcluded = false;
+//         isSolution = false;
+//         type = "occupations";
+//         constructor() {
+//             super();
+//         }
+//     },
+//     class Soldier extends Option {
+//         value = "Soldier";
+//         isInGame = true;
+//         isExcluded = false;
+//         isSolution = false;
+//         type = "occupations";
+//         constructor() {
+//             super();
+//         }
+//     },
+//     class Police extends Option {
+//         value = "Police";
+//         isInGame = true;
+//         isExcluded = false;
+//         isSolution = false;
+//         type = "occupations";
+//         constructor() {
+//             super();
+//         }
+//     }
+// ];
